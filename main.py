6def7f3c4b831c6cc0b83cabf07dc5d945fc2bf6
Raju <ankamma.raju.kunchala16@gmail.com>
2026-04-29 22:31:23 +0530

    graph

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 from dotenv import load_dotenv
 import os
 
+from langchain_core.messages import HumanMessage
+from langgraph.graph import END, MessagesState, StateGraph
+
+from nodes import run_agent_reasoning, tool_node
+
+
 load_dotenv()
 
-def main():
-    print("Hello from langgraph-course!")
-    
+AGENT_REASON = "agent_reasoning"
+ACT = "act"
+LAST = -1
+
+def should_continue(state: MessagesState) -> str:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END:END,
+    ACT:ACT
+})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 
 if __name__ == "__main__":
-    main()
+    print("Welcome to the LangGraph agent demo!")
+    res = app.invoke({"messages": [HumanMessage(content="what is the temperature in india? list it and then triple it?")]})
+    print("Agent response:", res["messages"][-1].content)
+
